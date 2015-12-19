bfac36a341525941865bd79fd935decfc08e0e11
Chris Dickenson <admin@webidsupport.com>
2015-12-19 21:18:21 +0000

    Wilks & sinclair graphs

--- app/Http/Controllers/ToolsController.php
+++ app/Http/Controllers/ToolsController.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Controller;
 use Auth;
 use App\Invite_code;
 use App\Log;
+use App\Log_item;
 
 class ToolsController extends Controller
 {
@@ -20,22 +21,39 @@ class ToolsController extends Controller
     public function bodyweight($range = 0)
     {
         $graphs = [];
-        $graphs['Bodyweight'] = Log::select('log_date', 'log_weight')
-                                        ->where('user_id', Auth::user()->user_id)
-                                        ->where('log_weight', '!=', 0)
-                                        ->orderBy('log_date', 'asc')
-                                        ->get();
+        $graphs['Bodyweight'] = Log::getbodyweight()->get();
         return view('tools.bodyweight', compact('range', 'graphs'));
     }
 
     public function wilks($range = 0)
     {
-        return view('tools.wilks', compact('range'));
+        $graph_data = Log_item::select('log_date', 'exercise_name', 'logitem_abs_weight as log_weight')
+                                ->join('exercises', 'log_items.log_id', '=', 'exercises.log_id')
+                                ->where('user_id', Auth::user()->user_id)
+                                ->where('is_pr', 1)
+                                ->whereIn('log_items.exercise_id', [Auth::user()->user_squatid, Auth::user()->user_deadliftid, Auth::user()->user_benchid])
+                                ->orderBy('log_date', 'asc');
+        $graphs = $graph_data->groupBy('exercise_name')->toArray();
+        $graphs['Bodyweight'] = Log::getbodyweight()->get();
+        $graphs['Wilks'] = []; //TODO ->map()
+        $graphs->map(function ($item, $key) {
+            return $item * 2;
+        });
+        return view('tools.wilks', compact('range', 'graphs'));
     }
 
     public function sinclair($range = 0)
     {
-        return view('tools.sinclair', compact('range'));
+        $graph_data = Log_item::select('log_date', 'exercise_name', 'logitem_abs_weight as log_weight')
+                                ->join('exercises', 'log_items.log_id', '=', 'exercises.log_id')
+                                ->where('user_id', Auth::user()->user_id)
+                                ->where('is_pr', 1)
+                                ->whereIn('log_items.exercise_id', [Auth::user()->user_snatchid, Auth::user()->user_cleanjerkid])
+                                ->orderBy('log_date', 'asc');
+        $graphs = $graph_data->groupBy('exercise_name')->toArray();
+        $graphs['Bodyweight'] = Log::getbodyweight()->get();
+        $graphs['Sinclair'] = []; //TODO
+        return view('tools.sinclair', compact('range', 'graphs'));
     }
 
     public function invites()
--- app/Log.php
+++ app/Log.php
@@ -8,4 +8,12 @@ class Log extends Model
 {
     protected $primaryKey = 'log_id';
     protected $dates = ['log_date'];
+
+    public function scopeGetbodyweight($query, $user_id)
+    {
+        return $query->select('log_date', 'log_weight')
+                    ->where('user_id', $user_id)
+                    ->where('log_weight', '!=', 0)
+                    ->orderBy('log_date', 'asc');
+    }
 }
