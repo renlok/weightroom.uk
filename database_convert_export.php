4b0fe9b8f519ad18e56bc6301af54bd557094465
Chris Dickenson <admin@webidsupport.com>
2016-01-03 17:43:37 +0000

    Fixed database converter

--- database_convert_export.php
+++ database_convert_export.php
@@ -37,8 +37,8 @@ $renamed = [
 
 foreach ($tables as $old_name => $table)
 {
-    $query = "SELECT * FROM $old_name;"
-    $result = $db->mysqli_query($query);
+    $query = "SELECT * FROM $old_name;";
+    $result = $db->query($query);
     while ($row = $result->fetch_assoc())
     {
         $keys = array_keys($row);
@@ -62,14 +62,14 @@ foreach ($tables as $old_name => $table)
             }
         }
         // print
-        echo "INSERT INTO $table ($colomns) VALUES ($values);\n"
+        echo "INSERT INTO $table ($colomns) VALUES ($values);\n";
     }
 }
 
 // calculate is_est1rm
-$query = "SELECT * FROM exercise_records ORDER BY ORDER BY user_id, exercise_id, pr_reps, pr_date ASC";
+$query = "SELECT * FROM exercise_records ORDER BY user_id, exercise_id, pr_reps, pr_date ASC";
 $user_id = $exercise_id = $pr_reps = $max = 0;
-$result = $db->mysqli_query($query);
+$result = $db->query($query);
 while ($row = $result->fetch_assoc())
 {
     // new set
@@ -79,38 +79,38 @@ while ($row = $result->fetch_assoc())
         $exercise_id = $row['exercise_id'];
         $pr_reps = $row['pr_reps'];
         $max = 0;
-        echo "UPDATE exercise_records SET is_est1rm = 1 WHERE pr_id = {$row['pr_id']};\n"
+        echo "UPDATE exercise_records SET is_est1rm = 1 WHERE pr_id = {$row['pr_id']};\n";
     }
-    elseif ($row['pr_1rm'] > $max;)
+    elseif ($row['pr_1rm'] > $max)
     {
         $max = $row['pr_1rm'];
-        echo "UPDATE exercise_records SET is_est1rm = 1 WHERE pr_id = {$row['pr_id']};\n"
+        echo "UPDATE exercise_records SET is_est1rm = 1 WHERE pr_id = {$row['pr_id']};\n";
     }
 }
 
 // count log totals
 $query = "SELECT log_id, SUM(logex_volume) as logex_volume, SUM(logex_reps) as logex_reps, SUM(logex_sets) as logex_sets FROM  `log_exercises` GROUP BY log_id";
-$result = $db->mysqli_query($query);
+$result = $db->query($query);
 while ($row = $result->fetch_assoc())
 {
-    echo "UPDATE logs SET log_total_volume = {$row['logex_volume']}, log_total_reps = {$row['logex_reps']}, log_total_sets = {$row['logex_sets']} WHERE log_id = {$row['log_id']};\n"
+    echo "UPDATE logs SET log_total_volume = {$row['logex_volume']}, log_total_reps = {$row['logex_reps']}, log_total_sets = {$row['logex_sets']} WHERE log_id = {$row['log_id']};\n";
 }
 
 // get failed volume
-$query = "SELECT SUM(logitem_weight*logitem_sets) as failedvolume, log_id FROM `log_items` WHERE logitem_reps = 0 GROUP BY log_id"
-$result = $db->mysqli_query($query);
+$query = "SELECT SUM(logitem_weight*logitem_sets) as failedvolume, log_id FROM `log_items` WHERE logitem_reps = 0 GROUP BY log_id";
+$result = $db->query($query);
 while ($row = $result->fetch_assoc())
 {
-    echo "UPDATE logs SET log_failed_volume = {$row['failedvolume']} WHERE log_id = {$row['log_id']};\n"
+    echo "UPDATE logs SET log_failed_volume = {$row['failedvolume']} WHERE log_id = {$row['log_id']};\n";
 }
 
 // get logex_id
 $query = "SELECT i.logitem_id, e.logex_id FROM `log_items` i
         JOIN log_exercises e ON (i.user_id = e.user_id AND i.log_id = e.log_id AND i.exercise_id = e.exercise_id)
         GROUP BY i.exercise_id, i.log_id";
-$result = $db->mysqli_query($query);
+$result = $db->query($query);
 while ($row = $result->fetch_assoc())
 {
-    echo "UPDATE log_items SET logex_id = {$row['logex_id']} WHERE logitem_id = {$row['logitem_id']};\n"
+    echo "UPDATE log_items SET logex_id = {$row['logex_id']} WHERE logitem_id = {$row['logitem_id']};\n";
 }
 $db->close();
