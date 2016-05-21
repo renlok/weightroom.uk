99e0b9984766c0cc2785e3de7bfdee05b5599db2
Chris Dickenson <renlok@users.noreply.github.com>
2016-05-22 00:17:28 +0100

    Update 2016_05_21_180800_create_import_data_table.php

--- database/migrations/2016_05_21_180800_create_import_data_table.php
+++ database/migrations/2016_05_21_180800_create_import_data_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateExerciseGoalsTable extends Migration
+class CreateImportDataTable extends Migration
 {
     /**
      * Run the migrations.
