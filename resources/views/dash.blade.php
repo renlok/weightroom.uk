35ef64cf5437b5265740f253ce6b6586b7fffc1b
Chris Dickenson <admin@webidsupport.com>
2016-12-21 21:15:19 +0000

    Improvements to dashboard

--- resources/views/dash.blade.php
+++ resources/views/dash.blade.php
@@ -16,10 +16,18 @@ blockquote.small {
 
 @section('content')
 <div class="padding-sb">
-<div class="btn-group btn-group-sm" role="group" aria-label="type">
-  <a href="{{ route('dashboard') }}" class="btn btn-default">Default</a>
-  <a href="{{ route('dashboardAll') }}" class="btn btn-default">View all</a>
-</div>
+	<div class="row">
+		<div class="pull-left">
+			Recent Activity
+		</div>
+		<div class="pull-right" style="margin-top: 20px;">
+			<div class="btn-group btn-group-sm" role="group" aria-label="type">
+				<a href="#" class="btn btn-default disabled" role="button">View:</a>
+			  <a href="{{ route('dashboard') }}" class="btn btn-default">Following</a>
+			  <a href="{{ route('dashboardAll') }}" class="btn btn-default">All</a>
+			</div>
+		</div>
+	</div>
 </div>
 @if ($random)
 <h3>You aren't currently following anyone</h3>
@@ -31,15 +39,15 @@ blockquote.small {
 	<tr>
 		<td class="logrow">
 			<a href="{{ route('viewUser', ['user_name' => $log->user->user_name]) }}">{{ $log->user->user_name }}</a> posted a log {{ (Carbon::now()->startOfDay()->eq($log->log_date)) ? 'Today' : $log->log_date->diffForHumans() }}. <a href="{{ route('viewLog', ['date' => $log->log_date->toDateString(), 'user_name' => $log->user->user_name]) }}">View log</a>
-            <blockquote class="small">{!! nl2br(htmlentities($log->log_text)) !!}</blockquote>
+      <blockquote class="small">{!! nl2br(htmlentities($log->log_text)) !!}</blockquote>
 		</td>
 	</tr>
 @empty
-    <tr>
-        <td class="logrow">
-            There has been no logs posted by people you follow
-        </td>
-    </tr>
+  <tr>
+    <td class="logrow">
+      There has been no logs posted by people you follow
+    </td>
+  </tr>
 @endforelse
 </tbody>
 </table>
