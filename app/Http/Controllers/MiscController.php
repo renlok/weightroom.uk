b151bfaff40b883387269e1f7b5bf80187352d54
Chris Dickenson <admin@webidsupport.com>
2017-01-22 18:22:13 +0000

    dash now shows when a log was posted

--- app/Http/Controllers/MiscController.php
+++ app/Http/Controllers/MiscController.php
@@ -14,51 +14,51 @@ use Illuminate\Http\Request;
 
 class MiscController extends Controller
 {
-	public function landing()
-	{
-		return view('landing');
-	}
+    public function landing()
+    {
+        return view('landing');
+    }
 
-	public function demo()
-	{
-		return view('demo');
-	}
+    public function demo()
+    {
+        return view('demo');
+    }
 
-	public function plans()
-	{
-		return view('plans');
-	}
+    public function plans()
+    {
+        return view('plans');
+    }
 
-	public function faq()
-	{
-		return view('help.faq');
-	}
+    public function faq()
+    {
+        return view('help.faq');
+    }
 
-	public function privacyPolicy()
-	{
-		return view('help.privacypolicy');
-	}
+    public function privacyPolicy()
+    {
+        return view('help.privacypolicy');
+    }
 
-	public function dash()
-	{
-		$followed_users = User_follow::where('user_id', Auth::user()->user_id)->pluck('follow_user_id');
-		$random = false;
-		if ($followed_users == null || $followed_users->count() == 0)
-		{
-			$followed_users = Cache::remember('random_users_dash', 360, function()
-			{
-			    return User::select(DB::raw('DISTINCT(users.user_id)'))->join('logs', 'users.user_id', '=', 'logs.user_id')->orderBy(\DB::raw('RAND()'))->take(10)->pluck('users.user_id');
-			});
-			$random = true;
-		}
-		$logs = Log::whereIn('user_id', $followed_users)->whereRaw("TRIM(log_text) != ''")->orderBy('log_date', 'desc')->paginate(50);
-		return view('dash', compact('logs', 'random'));
-	}
+    public function dash()
+    {
+        $followed_users = User_follow::where('user_id', Auth::user()->user_id)->pluck('follow_user_id');
+        $random = false;
+        if ($followed_users == null || $followed_users->count() == 0)
+        {
+            $followed_users = Cache::remember('random_users_dash', 360, function()
+            {
+                return User::select(DB::raw('DISTINCT(users.user_id)'))->join('logs', 'users.user_id', '=', 'logs.user_id')->orderBy(\DB::raw('RAND()'))->take(10)->pluck('users.user_id');
+            });
+            $random = true;
+        }
+        $logs = Log::whereIn('user_id', $followed_users)->whereRaw("TRIM(log_text) != ''")->orderBy('log_date', 'desc')->paginate(50);
+        return view('dash', compact('logs', 'random'));
+    }
 
-	public function dashAll()
-	{
-		$logs = Log::orderBy('log_date', 'desc')->whereRaw("TRIM(log_text) != ''")->paginate(50);
-		$random = false;
-		return view('dash', compact('logs', 'random'));
-	}
+    public function dashAll()
+    {
+        $logs = Log::orderBy('log_date', 'desc')->whereRaw("TRIM(log_text) != ''")->paginate(50);
+        $random = false;
+        return view('dash', compact('logs', 'random'));
+    }
 }
