88d082e9108aebe388377bf4b63ad247ac9322b3
Chris Dickenson <hopher10@googlemail.com>
2017-04-08 17:56:00 +0100

    Premium bugs fixed

--- resources/views/user/premium.blade.php
+++ resources/views/user/premium.blade.php
@@ -8,16 +8,16 @@
   @include('common.flash')
   <div class="subscription-box">
     @if (Auth::user()->subscribed('weightroom_gold') && Auth::user()->subscription('weightroom_gold')->onTrial())
-    <p>You are currently using a trial of WeightRoom Gold, this will expire at <code>{{ Auth::user()->subscription()->trial_ends_at->toDayDateTimeString() }}</code>.</p>
+    <p>You are currently using a trial of WeightRoom Gold, this will expire at <code>{{ Auth::user()->subscription('weightroom_gold')->trial_ends_at->toDayDateTimeString() }}</code>.</p>
     <p>We hope you are finding WeightRoom premium useful if not we would love if you would let us know why by sending us an <a href="mailto:chris@weightroom.uk">email</a>.</p>
     <a class="btn btn-default" href="{{ route('userCancelPremium') }}" role="button">Cancel Premium</a>
     @elseif (Auth::user()->subscribed('weightroom_gold') && Auth::user()->subscription('weightroom_gold')->onGracePeriod())
     <p>We are sorry you don't want your premium account any more. If you change your mind it is as easy as one click away.</p>
-    <p>Your WeightRoom Gold membership will expire at <code>{{ Auth::user()->subscription()->ends_at->toDayDateTimeString() }}</code>.</p>
+    <p>Your WeightRoom Gold membership will expire at <code>{{ Auth::user()->subscription('weightroom_gold')->ends_at->toDayDateTimeString() }}</code>.</p>
     <a class="btn btn-default" href="{{ route('userResumePremium') }}" role="button">Resume Premium</a>
     @elseif (Auth::user()->subscribed('weightroom_gold'))
     <p>We would like to give you a huge thank you for supporting us.</p>
-    <p>We hope you are finding WeightRoom premium useful if not we would love if you would let us know why by sending us an <a href="mailto:chris@weightroom.uk">email</a>.</p>
+    <p>We hope you are finding WeightRoom Gold useful if not we would love if you would let us know why by sending us an <a href="mailto:chris@weightroom.uk">email</a>.</p>
     <a class="btn btn-default" href="{{ route('userCancelPremium') }}" role="button">Cancel Premium</a>
     @else
     <p>Currently, during the beta stages of WeightRoom all features are free for everyone, but if you like what we are doing and would like to support us we would definitely appreciate it.</p>
