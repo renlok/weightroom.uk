a34c1ae5629027dbb6d74abe3f735f2053ff2a14
Chris Dickenson <hopher10@googlemail.com>
2019-01-06 16:48:38 +0000

    MIssing Illuminate\Http\Response

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests;
 use App\Http\Requests\TemplateRequest;
+use Illuminate\Http\Response;
 
 use Auth;
 use Carbon\Carbon;
